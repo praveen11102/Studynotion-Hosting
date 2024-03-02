239f6a8a23d849e06e5a063fdd0bdd5f769920cd
Praveen <patelpraveen11102@gmail.com>
2024-03-02 19:51:38 +0530

    First Commit StudyNotion

--- server/index.js
+++ server/index.js
@@ -25,9 +25,10 @@ database.connect()
 // Middlewares
 app.use(express.json())
 app.use(cookieParser())
+
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     credentials: true,
   })
 )
